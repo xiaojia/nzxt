6d08c9162a68058d23dd7147918ff2245f0e2da4
xiaojia <menmaojia@mucang.cn>
2023-10-22 11:39:23 +0800

    init

---
+++ main.js
@@ -0,0 +1,142 @@
+const timeDOM = document.getElementById('time');
+const weatherDOM = document.getElementById('weather');
+const cpuDOM = document.getElementById('cpu');
+const gpuDOM = document.getElementById('gpu');
+const ramDOM = document.getElementById('ram');
+
+window.nzxt = {
+    v1: {
+        onMonitoringDataUpdate: (data) => {
+            renderHardware(data);
+        }
+    }
+};
+
+const initHardware = () => {
+
+    const width = 60;
+    const bgColor = 'rgba(0,0,0,0.45)';
+    const rotateColor = '#D600BF';
+    const number = 1;
+    const show = '';
+
+    rotate('#cpu .circle').init({
+        number: number,
+        show: show,
+        rotateColor: rotateColor,
+        bgColor: bgColor,
+        width: width
+    });
+
+    rotate('#gpu .circle').init({
+        number: number,
+        show: show,
+        rotateColor: rotateColor,
+        bgColor: bgColor,
+        width: width
+    });
+
+}
+
+const renderHardware = (data) => {
+    if (data?.cpus?.length && data?.gpus?.length) {
+        rotate('#cpu .circle').init({
+            number: (data.cpus[0].load * 100) || 0.1,
+            show: parseInt(data.cpus[0].temperature)
+        });
+        rotate('#gpu .circle').init({
+            number: (data.gpus[0].load * 100) || 0.1,
+            show: parseInt(data.gpus[0].temperature)
+        });
+        ramDOM.style.width = (data.ram.inUse / data.ram.totalSize * 100) + '%';
+    }
+}
+
+const renderBackground = () => {
+
+    let oldBackground = document.getElementById('background');
+    let background = document.createElement('div');
+    let src = `../background/${Math.round(Math.random() * 171 + 1)}.jpg`;
+    let img = new Image();
+
+    background.classList.add('background');
+    background.style.backgroundImage = `url(../background/${Math.round(Math.random() * 171 + 1)}.jpg)`;
+    background.style.zIndex = 1;
+    background.id = 'background'
+
+    document.body.appendChild(background);
+
+    img.src = src;
+    img.onload = () => {
+        background.style.opacity = 1;
+        setTimeout(() => {
+            if (oldBackground) {
+                document.body.removeChild(oldBackground);
+            }
+        }, 1000);
+    }
+
+}
+
+const fillZero = (n) => {
+    return n < 10 ? '0' + n : n;
+}
+
+const renderTime = () => {
+    let date = new Date();
+    timeDOM.innerHTML = `${fillZero(date.getHours())}:${fillZero(date.getMinutes())}`;
+}
+
+const renderWeather = (data) => {
+    fetch('https://restapi.amap.com/v3/weather/weatherInfo?key=327ea5105343e1356319ec48b91036fe&city=420115&extensions=base').then(async (response) => {
+        if (response.ok) {
+            let data = (await response.json())?.lives[0];
+            if (data) {
+                weatherDOM.innerHTML = `<span>${data.temperature}</span>${data.weather}`;
+            }
+        }
+    });
+}
+
+const mainBackground = () => {
+    renderBackground();
+    setInterval(renderBackground, 60 * 1000);
+}
+
+const mainTime = () => {
+    renderTime();
+    setInterval(renderTime, 5 * 1000);
+}
+
+const mainWeather = () => {
+    renderWeather();
+    setInterval(renderWeather, 60 * 1000);
+}
+
+const mainHardware = () => {
+
+    initHardware();
+
+    if (location.search.indexOf('kraken=1') === -1) {
+        renderHardware({
+            cpus: [{
+                load: 0.004368910565972328,
+                temperature: 60
+            }],
+            gpus: [{
+                load: 0.004368910565972328,
+                temperature: 50
+            }],
+            ram: {
+                totalSize: 65268,
+                inUse: 13076,
+            }
+        });
+    }
+
+}
+
+// mainBackground();
+mainTime();
+mainWeather();
+mainHardware();
---
+++ rotate.js
@@ -0,0 +1,83 @@
+; (function (w, d) {
+	function R() {
+		this.dom = typeof arguments[0] == 'object' ? [arguments[0]] : d.querySelectorAll(arguments[0]);
+		this.s = {
+			number: null,
+			width: null,
+			bgColor: null,
+			rotateColor: null,
+			space: 10,
+			speed: 1000,
+			hide: false
+		};
+	};
+	R.prototype = {
+		init: function (o) {
+			var t = this;
+			this.extend(this.s, o);
+			this.create();
+			;[].forEach.call(this.dom, function (o) {
+				var n = t.s.number ? t.s.number : o.dataset.num;
+				if (!n) { alert('no number'); return }
+				var num = 'rotate(' + n * 1.8 + 'deg)';
+				var s = 4;
+				if (+n == 0 && n < 96) { s = 0; };
+				if (n >= 96 && n < 100) { s = 1 } else if (n >= 100) { s = 0 };
+				var num2 = 'rotate(' + (n * 1.8 + s) + 'deg)';
+				o.querySelector('.progress').innerHTML = t.s.show;
+				var dom1 = o.querySelector('.rotate-f');
+				var dom2 = o.querySelector('.rotate-f .rotate-c');
+				var dom3 = o.querySelector('.rotate-h .rotate-c');
+				t.trans2(dom1, 'transform: rotate(0deg);');
+				t.trans2(dom2, 'transform: rotate(0deg);');
+				t.trans2(dom3, 'transform: rotate(0deg);');
+				t.trans(dom1, 0); t.trans(dom2, 0); t.trans(dom3, 0);
+				var dom4 = o.querySelector('.rotate-h');
+				if (t.s.rotateColor) {
+					dom2.style.background = dom3.style.background = t.s.rotateColor;
+				};
+				dom1.style.clip = dom4.style.clip = 'rect(0, ' + o.offsetWidth + 'px, ' + o.offsetWidth + 'px,' + o.offsetWidth / 2 + 'px)';
+
+				setTimeout(function () {
+					t.trans(dom1, t.s.speed / 1000); t.trans(dom2, t.s.speed / 1000); t.trans(dom3, t.s.speed / 1000);
+					t.trans2(dom1, num); t.trans2(dom2, num);
+					t.trans2(dom3, num2);
+				}, 30);
+			});
+		},
+		trans: function (o, s) {
+			o.style.transition = o.style.MozTransition = o.style.WebkitTransition = 'all ' + s + 's ease-in-out';
+		},
+		trans2: function (o, s) {
+			o.style.transform = o.style.MozTransform = o.style.WebkitTransform = s;
+		},
+		create: function () {
+			var t = this;
+			;[].forEach.call(this.dom, function (o) {
+				if (t.s.width) {
+					o.style.width = t.s.width + (t.s.width.toString().indexOf('%') > 0 ? '' : 'px');
+					o.style.width = (o.offsetWidth % 2 == 0 ? o.offsetWidth : (o.offsetWidth + 1)) + 'px';
+					o.style.height = o.offsetWidth + 'px';
+				};
+				if (t.s.bgColor) {
+					o.style.background = t.s.bgColor;
+				};
+				if (!o.querySelector('.rotate-h')) {
+					o.classList.add('progress-circle');
+					o.insertAdjacentHTML("beforeend", '<span class="rotate-h"><span class="rotate-c"></span></span><span class="rotate-f"><span class="rotate-c"></span></span><span class="progress"></span>');
+				};
+				var pro = o.querySelector('.progress');
+				if (t.s.space) {
+					pro.style.width = pro.style.height = (o.offsetWidth - Math.floor(t.s.space * 2)) + 'px';
+				};
+				pro.style.opacity = t.s.hide ? 0 : 1
+				pro.style.lineHeight = (o.offsetWidth - t.s.space * 2) + 'px';
+
+			});
+		},
+		extend: function (n, n1) {
+			for (var i in n1) { n[i] = n1[i] };
+		}
+	};
+	function r(o) { return new R(o) }; w.rotate = r;
+})(window, document);
